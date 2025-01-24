be75c81dd18eb7cd1feab2637730613684e121f8
Justinne Baltazar <jfb3@sfu.ca>
2025-01-23 22:38:45 -0800

    Add auto drop for tetromino

--- src/components/GameController/GameController.jsx
+++ src/components/GameController/GameController.jsx
@@ -2,8 +2,24 @@ import React from "react";
 import styles from "./GameController.module.css";
 import { Action, actionForKey } from "../../utils/Input";
 import { playerController } from "../../utils/PlayerController";
+import { useInterval } from "../../hooks/useInterval";
+import { useDropTime } from "../../hooks/useDropTime";
 
 export const GameController = ({ board, gameStats, player, setGameOver, setPlayer }) => {
+
+    const [dropTime, pauseDropTime, resumeDropTime] = useDropTime({
+        board, 
+        gameStats, 
+        player, 
+        setGameOver, 
+        setPlayer
+    });
+ 
+    useInterval(() => {
+        handleInput({ action: Action.SlowDrop });
+    }, 1000);
+    // every second handle input is called
+    
     const onKeyUp = ({ code }) => {
         const action = actionForKey(code); 
 
