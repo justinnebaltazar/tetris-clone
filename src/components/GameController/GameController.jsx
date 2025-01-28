8a76f02fbc84abccae1feeb41915c01267b8f64c
Justinne Baltazar <jfb3@sfu.ca>
2025-01-27 20:35:54 -0800

    Add pause and quit game input handling

--- src/components/GameController/GameController.jsx
+++ src/components/GameController/GameController.jsx
@@ -1,55 +1,67 @@
 import React from "react";
 import styles from "./GameController.module.css";
-import { Action, actionForKey } from "../../utils/Input";
+import { Action, actionForKey, actionIsDrop } from "../../utils/Input";
 import { playerController } from "../../utils/PlayerController";
 import { useInterval } from "../../hooks/useInterval";
 import { useDropTime } from "../../hooks/useDropTime";
 
-export const GameController = ({ board, gameStats, player, setGameOver, setPlayer }) => {
-
+export const GameController = ({
+    board,
+    gameStats,
+    player,
+    setGameOver,
+    setPlayer
+  }) => {
     const [dropTime, pauseDropTime, resumeDropTime] = useDropTime({
-        board, 
-        gameStats, 
-        player, 
-        setGameOver, 
-        setPlayer
+      gameStats
     });
- 
+  
     useInterval(() => {
-        handleInput({ action: Action.SlowDrop });
-    }, 1000);
-    // every second handle input is called
-    
+      handleInput({ action: Action.SlowDrop });
+    }, dropTime);
+  
     const onKeyUp = ({ code }) => {
-        const action = actionForKey(code); 
-
-        if (action == Action.Quit) {
-            setGameOver(true);
-        }
-    }
-
+      const action = actionForKey(code);
+      if (actionIsDrop(action)) resumeDropTime();
+    };
+  
     const onKeyDown = ({ code }) => {
-        const action = actionForKey(code);
+      const action = actionForKey(code);
+  
+      if (action === Action.Pause) {
+        if (dropTime) {
+          pauseDropTime();
+        } else {
+          resumeDropTime();
+        }
+      } else if (action === Action.Quit) {
+        setGameOver(true);
+      } else {
+        if (actionIsDrop(action)) pauseDropTime();
+        if (!dropTime) {
+          return;
+        }
         handleInput({ action });
+      }
     };
-
+  
     const handleInput = ({ action }) => {
-        playerController({
-            action,
-            board, 
-            player,
-            setPlayer, 
-            setGameOver
-        }); 
+      playerController({
+        action,
+        board,
+        player,
+        setPlayer,
+        setGameOver
+      });
     };
-
+  
     return (
-       <input 
-       className={styles.GameController}
-       type="text"
-       onKeyDown={onKeyDown}
-       onKeyUp={onKeyUp}
-       autoFocus
-       ></input>
-    )
-}
+      <input
+        className={styles.GameController}
+        type="text"
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        autoFocus
+      />
+    );
+  };
